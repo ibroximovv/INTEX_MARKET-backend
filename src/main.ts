4d8b66a9c3de9f3cb86c2def0fffda8bb1c7425c
Ilyosbek <ilyosbekibroximov23z@gmail.com>
2025-07-21 16:26:52 +0500

    +

--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,50 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
-import * as express from "express";
+import * as express from 'express';
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('My API')
+    .setDescription('API description')
     .setVersion('1.0').addSecurityRequirements('bearer').addBearerAuth()
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    forbidNonWhitelisted: true,
-    whitelist: true
-  }));
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
+
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://server.rtudarsjadvali.uz',
+    'https://server.rtudarsjadvali.uz',
+  ];
+
 
   app.enableCors({
-    origin: '*', 
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
 
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
